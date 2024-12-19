a4bab6b71d40077431b6493ef78dbf0f2adf1108
Brian Yang <yangbri1@msu.edu>
2024-12-19 22:06:41 +0000

    attempted at a truncated way using Java built-in Arrays.asList() to convert array 'arr' into a List --- so can use .contains() method from List (it failed as List only takes in Object types & in our case it was taking the WHOLE int[] as 1 single elem into the List)

--- src/main/java/DoesNotContain.java
+++ src/main/java/DoesNotContain.java
@@ -15,19 +15,25 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-
+        
         // initialize variable 'notContain' w/ default value of true
         boolean notContain = true;
         // utilize for-each loop to iterate through each 'elem' in int array 'arr'
         for(int elem : arr){
             // if 'elem' does equals 'target' value ...
-            if(elem == target){
+            if(elem == target){     // Note: '==' operator works here as we are comparing primitive types (not complex Object types)
                 notContain = false;
             }
             // otherwise 'notContain' stays true
         }
         // return statement outside else errors
         return notContain;
+        
 
+        // nevermind this truncated way does NOT seem to work as java.util.Arrays.asList() here creates an List<int[]> --- aka a List w/ 1 elem of int array
+        // https://www.geeksforgeeks.org/arrays-aslist-method-in-java-with-examples/
+        // https://www.geeksforgeeks.org/list-contains-method-in-java-with-examples/
+        // boolean aryContain = java.util.Arrays.asList(arr).contains(target);
+        // return(!(aryContain));
     }
 }
